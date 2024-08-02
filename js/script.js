0be8c0bd487185f969062b6ee80febbd3877e362
Rahuldhiman2002 <115942477+Rahuldhiman2002@users.noreply.github.com>
2024-08-02 15:48:02 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getsongs(folder) {
   currfolder = folder;
-  let a = await fetch(`http://127.0.0.1:5502/${folder}/`);
+  let a = await fetch(`https://github.com/Rahuldhiman2002/My-Spotify-clone/${folder}/`);
   console.log(currfolder);
   let response = await a.text();
   let div = document.createElement("div");
